8ad863e2a177d9358f55d410faac64366c6af217
Isaac Owens <isaac.owens832@gmail.com>
2020-08-26 11:41:38 -0400

    Create sortByOptions object

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Search.css';
+
+const sortByOptions = {
+  "Best Match": 'best_match',
+  "Highest Rated": 'rating',
+  "Most Reviewed": 'review_count',
+}
+
+class SearchBar extends React.Component {
+  render() {
+    return(
+
+    )
+  }
+}
