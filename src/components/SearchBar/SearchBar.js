438c899a3277fffa6b9e0fa14fa0aa100fe52419
Isaac Owens <isaac.owens832@gmail.com>
2020-08-26 16:02:44 -0400

    Add sort option class method

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -22,7 +22,7 @@ class SearchBar extends React.Component {
     }
   }
 
-  getSortByclass(sortByOption) {
+  getSortByClass(sortByOption) {
     if (this.state.sortBy === sortByOption) {
       return 'active';
     } else {
@@ -30,10 +30,17 @@ class SearchBar extends React.Component {
     }
   };
 
+  handleSortByChange(sortByOption) {
+    this.setState({ sortBy: sortByOption });
+  }
+
   renderSortByOptions() {
     return Object.keys(this.sortByOptions).map(sortByOption => {
       let sortByOptionValue = this.sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>
+      return <li 
+      key={sortByOptionValue}
+      className={getSortByClass(sortByOptionValue)}
+      >{sortByOption}</li>
     })
   }
 
