d42849d26cafc035769b48821438af5f4db74ffb
Isaac Owens <isaac.owens832@gmail.com>
2020-08-27 13:37:10 -0400

    Add state and constructor to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,8 +6,16 @@ import SearchBar from '../SearchBar/SearchBar';
 import Yelp from '../../util/Yelp';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    }
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy) {
-    console.log('Searching Yelp with Pizza, Brooklyn, best_match');
+    
   }
 
   render() {
