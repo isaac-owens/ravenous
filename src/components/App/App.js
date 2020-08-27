e15d11dbfb11d3e16a2ef9566dc9d8951e478228
Isaac Owens <isaac.owens832@gmail.com>
2020-08-27 13:35:48 -0400

    Remove hardcoded businesses and business object in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,27 +3,7 @@ import './App.css';
 
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
-
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-]
+import Yelp from '../../util/Yelp';
 
 class App extends React.Component {
   searchYelp(term, location, sortBy) {
